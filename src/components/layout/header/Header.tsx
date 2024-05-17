b8cbda9eb2bb49fa26bd2669648dc8c0b81de0aa
Nick Mosher <nick@mosher.me>
2024-05-16 23:33:51 -0500

    feat: full screen header on mobile and some tablets

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -2,8 +2,8 @@ import { PropsWithChildren } from "react"
 
 const Header = ({ children }: PropsWithChildren) => {
   return (
-    <div className="h-72 md:h-96 lg:h-96 bg-brown-900 bg-hero-1 bg-no-repeat bg-cover bg-top xs:bg-top-1 md:bg-top-2 lg:bg-top-3 max-w-screen-xl m-auto">
-      <div className="backdrop-blur-xs h-full bg-brown-900/80 	">
+    <div className="h-screen lg:h-96 bg-brown-900 bg-hero-1 bg-no-repeat bg-cover bg-top lg:bg-top-3 max-w-screen-xl m-auto transition-all duration-[5000ms] hover:bg-bottom">
+      <div className="backdrop-blur-xs h-full bg-brown-900/80 content-center">
         {children}
       </div>
     </div>
--- src/components/layout/header/HeaderTitle.tsx
+++ src/components/layout/header/HeaderTitle.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 const HeaderTitle = ({ children }: Props) => {
   return (
-    <h1 className='font-serif text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold text-brown-50 drop-shadow text-center pb-5 pt-40'>
+    <h1 className='font-serif text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold text-brown-50 drop-shadow text-center pb-5'>
         {children}
     </h1>
   )
