37bb7e8ea9d1bd21444b8404b46a9f22422cc956
Nick Mosher <nick@mosher.me>
2024-05-25 22:26:10 -0500

    feat: smooth scroll added

--- src/components/layout/header/digital-native/Header.tsx
+++ src/components/layout/header/digital-native/Header.tsx
@@ -1,18 +1,24 @@
 import { PropsWithChildren } from "react"
 import ScrollDownIcon from "../../../ScrollDownIcon"
 import TopNav from "../../../TopNav";
+import { animateScroll as scroll } from 'react-scroll';
 
 interface Props {
   scrollLink?: string;
 }
 
 const Header = ({ scrollLink, children }: PropsWithChildren<Props>) => {
+  
+  const scrollMore = () => {
+    scroll.scrollMore(500); // Scrolling an additional 500px from the current scroll position.
+  };
+  
   return (
     <div className={`relative h-screen lg:h-96 bg-digital-native-bg bg-hero-digital-native bg-no-repeat bg-cover bg-top lg:bg-top-3 max-w-screen-xl m-auto transition-[background] duration-[20000ms] hover:bg-bottom motion-reduce:transition-none motion-reduce:hover:br-top lg:rounded-b-2xl`}>
       <TopNav active="digital-native" menuColor="text-white" />
       <div className="backdrop-blur-xs h-full bg-digital-native-bg content-center lg:rounded-b-2xl">
         {children}
-        <a className="absolute inset-x-0 bottom-0 w-screen pb-2 lg:hidden opacity-50" href={scrollLink}><ScrollDownIcon className="m-auto" stroke="#FFFFFF"/></a>
+        <a className="absolute inset-x-0 bottom-0 w-screen pb-2 lg:hidden opacity-50" href={scrollLink} onClick={scrollMore}><ScrollDownIcon className="m-auto" stroke="#FFFFFF"/></a>
       </div>
     </div>
   )
