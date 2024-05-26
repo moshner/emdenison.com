bc822a3bb4c805406e47960c10e7b3f25eb440e1
Nick Mosher <nick@mosher.me>
2024-05-25 22:34:26 -0500

    fix: removed unused component

--- src/components/SectionHeader.1.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export const SectionHeader = ({ children }: PropsWithChildren) => {
-  return (
-      <h2 className="pt-40 pb-10 font-sans-serif text-3xl font-medium text-brown-50 drop-shadow text-center">{children}</h2>
-  );
-};
