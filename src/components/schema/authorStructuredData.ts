11c13b8eefdbcfd8cac4d9a01f018d993b33c420
Nick Mosher <nick@mosher.me>
2025-12-09 23:33:29 -0600

    fix: bio update

--- src/components/schema/authorStructuredData.ts
+++ src/components/schema/authorStructuredData.ts
@@ -7,7 +7,7 @@
     "jobTitle": "Author",
     "name": "E.M. Denison",
     "url": "https://emdenison.com",
-    "description": "E.M Denison is a sci-fi and fantasy author who favors satire and stories featuring spunky robots. She grew up in Kansas and played a lot of trombone in high school and college. She studied geology in college and lived in a paleontology lab measuring Devonian lungfish burrows and the fungus holes in Cretaceous wood. She has worked as an engineering geologist, a science journalist, and a medical grant writer. In 2010, She married the love of her  life, and they have three hilarious little kids and three ultra-needy cats.",
+    "description": "E.M Denison is a sci-fi and fantasy author who favors satire and stories featuring spunky robots. She grew up in Kansas and played a lot of trombone in high school and college. She studied geology in college and lived in a paleontology lab measuring Devonian lungfish burrows and the fungus holes in Cretaceous wood. She has worked as an engineering geologist, a science journalist, and a medical grant writer. In 2010, She married the love of her life, and they have four hilarious kids and three ultra-needy cats.",
     "mainEntityOfPage": {
         "@type": "WebPage",
         "@id": "https://emdenison.com/"
